d1bb1b90f91bca36243262864f22e3a327df3b8e
Michael <mpladow@gmail.com>
2022-01-14 22:15:17 +1100

    Welcome commit

---
+++ screens/WelcomeStack/Welcome.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View,Text } from '../../components/Themed';
+
+
+const Welcome = () => {
+
+	return (
+		<View>
+			<Text>Test text</Text>
+		</View>
+	);
+
+}
+
+export default Welcome;
+
